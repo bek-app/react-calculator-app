84b1c240a166279a4d4081547d7ef1fe69b2adac
bek-app <make202006@gmail.com>
2021-06-05 02:37:42 +0600

    add operators add,subtract,myltiply, divide

--- src/App.js
+++ src/App.js
@@ -30,6 +30,55 @@ class App extends Component {
     clearInput = (val) => {
         this.setState({ input: "" });
     };
+    add = (val) => {
+        this.state.previousNumber = this.state.input;
+        this.setState({ input: "" });
+        this.state.operator = "plus";
+    };
+    subtract = (val) => {
+        this.state.previousNumber = this.state.input;
+        this.setState({ input: "" });
+        this.state.operator = "subtract";
+    };
+    multiply = (val) => {
+      this.state.previousNumber = this.state.input;
+      this.setState({ input: "" });
+      this.state.operator = "multiply";
+    };
+    divide = (val) => {
+      this.state.previousNumber = this.state.input;
+      this.setState({ input: "" });
+      this.state.operator = "divide";
+    };
+    evaluate = (val) => {
+        this.state.currentNumber = this.state.input;
+        if (this.state.operator === "plus") {
+            this.setState({
+                input:
+                    parseInt(this.state.previousNumber) +
+                    parseInt(this.state.currentNumber),
+            });
+        } else if (this.state.operator === "subtract") {
+            this.setState({
+                input:
+                    parseInt(this.state.previousNumber) -
+                    parseInt(this.state.currentNumber),
+            });
+        } else if (this.state.operator === "multiply") {
+            this.setState({
+                input:
+                    parseInt(this.state.previousNumber) *
+                    parseInt(this.state.currentNumber),
+            });
+        } else if (this.state.operator === "divide") {
+            this.setState({
+                input:
+                    parseInt(this.state.previousNumber) /
+                    parseInt(this.state.currentNumber),
+            });
+        }
+    };
+
     render() {
         return (
             <div className="App">
@@ -41,25 +90,25 @@ class App extends Component {
                         <Button handleClick={this.addToInput}>7</Button>
                         <Button handleClick={this.addToInput}>8</Button>
                         <Button handleClick={this.addToInput}>9</Button>
-                        <Button handleClick>/</Button>
+                        <Button handleClick={this.divide}>/</Button>
                     </div>
                     <div className="row">
                         <Button handleClick={this.addToInput}>4</Button>
                         <Button handleClick={this.addToInput}>5</Button>
                         <Button handleClick={this.addToInput}>6</Button>
-                        <Button>*</Button>
+                        <Button handleClick={this.multiply}>*</Button>
                     </div>
                     <div className="row">
                         <Button handleClick={this.addToInput}>1</Button>
                         <Button handleClick={this.addToInput}>2</Button>
                         <Button handleClick={this.addToInput}>3</Button>
-                        <Button handleClick={this.addToInput}>+</Button>
+                        <Button handleClick={this.add}>+</Button>
                     </div>
                     <div className="row">
                         <Button handleClick={this.addDecimal}>.</Button>
                         <Button handleClick={this.addZeroToInput}>0</Button>
-                        <Button handleClick={this.addDecimal}>=</Button>
-                        <Button handleClick={this.addToInput}>-</Button>
+                        <Button handleClick={this.evaluate}>=</Button>
+                        <Button handleClick={this.subtract}>-</Button>
                     </div>
                     <div className="row">
                         <ClearButton
